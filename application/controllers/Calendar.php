eded6be0e3cfff5dbd35da28b8d9232fb4c31e8d
Snowman1030 <web.developer0221@gmail.com>
2021-04-16 21:00:41 +0900

    fix

--- application/controllers/Calendar.php
+++ application/controllers/Calendar.php
@@ -20,7 +20,7 @@ class Calendar extends CI_Controller {
 	 */
 	public function index()
 	{
-		 $this->load->view('header');
+		$this->load->view('header');
         $this->load->view('calendar');
         $this->load->view('footer');
 	}
