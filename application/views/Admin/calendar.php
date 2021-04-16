058f827b5bce2df7b8895cedbe9d346859030ae0
Snowman1030 <web.developer0221@gmail.com>
2021-04-16 20:32:48 +0900

    admin

---
+++ application/views/Admin/calendar.php
@@ -0,0 +1,21 @@
+<body>
+
+  <div class="response"></div>
+    <div class="content-wrapper">
+      <div class="container-fluid">
+        
+        <div id='calendar'></div>
+        <div class="overlay toggle-menu"></div>
+      </div>
+    </div>
+    
+    
+    <!--End footer-->
+
+
+
+  </div>
+  <!--End wrapper-->
+</body>
+
+</html>
---
+++ application/views/Admin/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="utf-8"/>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+      <meta name="description" content=""/>
+      <meta name="author" content=""/>
+      <title>Dashtreme - Multipurpose Bootstrap4 Admin Template</title>
+      <link href="assets/plugins/fullcalendar/css/fullcalendar.css" rel='stylesheet'/>
+      <!-- simplebar CSS-->
+      <link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet"/>
+      <!-- Bootstrap core CSS-->
+      <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+      <!-- animate CSS-->
+      <link href="assets/css/animate.css" rel="stylesheet" type="text/css"/>
+      <!-- Icons CSS-->
+      <link href="assets/css/icons.css" rel="stylesheet" type="text/css"/>
+      <!-- Horizontal menu CSS-->
+      <link href="assets/css/horizontal-menu.css" rel="stylesheet"/>
+      <!-- Custom Style-->
+      <link href="assets/css/app-style.css" rel="stylesheet"/>
+    </head>
+    <style>
+body {
+    margin-top: 50px;
+    text-align: center;
+    font-size: 12px;
+    font-family: "Lucida Grande", Helvetica, Arial, Verdana, sans-serif;
+}
+
+#calendar {
+    width: 700px;
+    margin: 0 auto;
+}
+
+.response {
+    height: 60px;
+}
+
+.success {
+    background: #cdf3cd;
+    padding: 10px 60px;
+    border: #c3e6c3 1px solid;
+    display: inline-block;
+}
+</style>
+    
